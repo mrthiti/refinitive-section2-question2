99367e6c72b88804cd5c4d50a929f5837f576462
Thiti Yamsung <t.yamsung@gmail.com>
2021-03-27 14:56:37 +0700

    Implement main feature

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      Filter: <input type="text" v-model="filterText" />
+    </div>
+    <div class="vertical-space" v-if="!error">
+      
+      <table class="center">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in filterCategorys" :key="index">
+            <td>{{ index + 1 }}</td>
+            <td style="text-align: left;">{{ item }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+    <div class="vertical-space" v-else>
+      {{ error }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      filterText: '',
+      categorys: [],
+      error: null
+    }
+  },
+  computed:{
+    filterCategorys: function(){
+      let locFiltertext = this.filterText
+
+      if(!locFiltertext) return this.categorys
+
+      return this.categorys.filter( function(item) {
+        return item.toLowerCase().includes(locFiltertext.toLowerCase())
+      })
+    }
+  },
+  async mounted(){
+    this.error = null
+
+    try{
+      let { data: categoryResponse } = await axios.get('https://api.publicapis.org/categories')
+      console.log(categoryResponse)
+      this.categorys = categoryResponse
+    }catch(err){
+      console.log('Error')
+      this.error = "Not get data category!!"
+    }
   }
 }
 </script>
@@ -24,5 +73,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.vertical-space {
+  margin-top: 10px;
 }
 </style>
